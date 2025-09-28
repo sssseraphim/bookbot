7085a75bec482df3a0fbfd47d50a70e426212bd5
sssseraphim <tarasovv2004@ya.ru>
2025-09-28 21:25:19 +0300

    added a report

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from stats import count_words
+from stats import count_words, count_characters, sorted_dicts
+import sys
 
 def get_books_text(filepath: str) -> str:
     with open(filepath) as f:
         return f.read()
 
 def main():
-    text = get_books_text("books/frankenstein.txt")
+    files = sys.argv
+    if len(files) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    book = files[1]
+    text = get_books_text(files[1])
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}...")
+    print("----------- Word Count ----------")
     print(f"Found {count_words(text)} total words")
+    print("--------- Character Count -------")
+    for d in sorted_dicts(count_characters(text)):
+        if d["char"].isalpha():
+            print(f"{d["char"]}: {d["num"]}")
+    print("============= END ===============")
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,2 +1,23 @@
 def count_words(text: str) -> int:
     return len(text.split())
+
+
+def count_characters(text: str) -> int:
+    f = {}
+    for ch in text:
+        ch = ch.lower()
+        f[ch] = f.get(ch, 0) + 1
+    return f
+
+
+def sort_on(items):
+    return items["num"]
+
+
+def sorted_dicts(d):
+    res = []
+    for k in d:
+        res.append({"char": k, "num": d[k]})
+    res.sort(reverse=True, key=sort_on)
+    return res
+    
